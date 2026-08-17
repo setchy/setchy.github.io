bec10e572ee60d2b126d8f16e44a7f8974a2544a
Adam Setch <adam.setch@outlook.com>
2026-08-17 10:59:51 -0400

    feat: extra data
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

---
+++ src/data/nav.ts
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { label: 'About', href: '/about/' },
+  { label: 'OSS', href: '/open-source' },
+  { label: 'Industry', href: '/industry' },
+  { label: 'Radars', href: '/radars' },
+  { label: 'Library', href: '/library' },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,9 @@
+export const socials = [
+  { name: 'RSS', href: '/feed.xml', icon: 'fa-solid fa-rss' },
+  { name: 'Facebook', href: 'https://www.facebook.com/adam.setch', icon: 'fa-brands fa-facebook' },
+  { name: 'GitHub', href: 'https://github.com/setchy', icon: 'fa-brands fa-github' },
+  { name: 'X (Twitter)', href: 'https://x.com/setchy87', icon: 'fa-brands fa-x-twitter' },
+  { name: 'YouTube', href: 'https://www.youtube.com/setchy87', icon: 'fa-brands fa-youtube' },
+  { name: 'LinkedIn', href: 'https://linkedin.com/in/adamsetch', icon: 'fa-brands fa-linkedin-in' },
+  { name: 'Untappd', href: 'https://untappd.com/user/setchy', icon: 'fa-brands fa-untappd' },
+];
