c63bad3379b6bf047cd0817f3bff26852942565c
Adam Setch <adam.setch@outlook.com>
2026-09-19 12:12:46 -0400

    fix: menu on mobile
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -4,6 +4,7 @@ export interface NavLink {
 }
 
 export const navLinks: NavLink[] = [
+  { label: 'Home', href: '/' },
   { label: 'About', href: '/about/' },
   { label: 'OSS', href: '/open-source/' },
   { label: 'Industry', href: '/industry/' },
